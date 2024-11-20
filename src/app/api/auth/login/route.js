55731923469e0bc4505a887d96d5a99ca0ae450a
AMITHRAJ V <102154967+amith-mojgenie@users.noreply.github.com>
2024-11-20 14:38:23 +0530

    auth/login api

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,13 +1,31 @@
+import connect from '@/dbConfig/dbConfig';
+import User from '@/models/userModal';
 import { NextResponse } from 'next/server';
+
+connect();
 
 export async function POST(request) {
   try {
     const reqBody = await request.json();
-    const { userId, password } = reqBody;
+    const { userid, password } = reqBody;
+
+    // Check if user already exists
+    const user = await User.findOne({ userid });
+
+    if (user) {
+      return NextResponse.json(
+        { error: 'User already exists' },
+        { status: 400 }
+      );
+    }
+    //Add user to the db
+    const newUser = new User({ userid, password });
+    const savedUser = await newUser.save();
 
     return NextResponse.json({
-      message: 'succes',
+      message: 'User created successfully',
       success: true,
+      savedUser,
     });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
---
+++ src/models/userModal.js
@@ -0,0 +1,16 @@
+const { Schema, mongoose } = require('mongoose');
+
+const userSchema = new Schema({
+  userid: {
+    type: String,
+    require: [true, 'Please provide a userid'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: [true, 'Please provide a password'],
+  },
+});
+
+const User = mongoose.models.users || mongoose.model('users', userSchema);
+export default User;
