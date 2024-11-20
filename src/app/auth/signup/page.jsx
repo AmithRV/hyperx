afcdb9696d6600cc34d34341f7117ee7a3de5c54
AMITHRAJ V <102154967+amith-mojgenie@users.noreply.github.com>
2024-11-20 14:54:15 +0530

    signup api connect

--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useFormik } from 'formik';
+import toast, { Toaster } from 'react-hot-toast';
 
 import ErrorMessage from '@/app/components/ui/form/ErrorMessage';
 
@@ -30,7 +31,16 @@ function Signup() {
         password: formik.values.password,
       })
       .then(() => {})
-      .catch(() => {});
+      .catch((error) => {
+        if (error.response.status === 400) {
+          toast.error(error.response.data.error);
+        } else {
+          toast.error('something went wrong');
+        }
+      })
+      .finally(() => {
+        setLoading({ type: 'login', state: false });
+      });
   };
 
   const formik = useFormik({
@@ -50,44 +60,51 @@ function Signup() {
   };
 
   return (
-    <div className="auth_section_1">
-      <form className="form_section" onSubmit={handleSubmit}>
-        <div className="form-group mx-4 my-4">
-          <label htmlFor="exampleInputEmail1">UserId</label>
-          <input
-            type="text"
-            className="form-control mt-2 fw-bold"
-            name="userId"
-            onChange={formik.handleChange}
-          />
-          <ErrorMessage
-            message={formik.errors.userId}
-            name="userId"
-            formik={formik}
-          />
-        </div>
-        <div className="form-group mx-4 mt-4 mb-3">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            className="form-control mt-2 fw-bold"
-            name="password"
-            onChange={formik.handleChange}
-          />
-          <ErrorMessage
-            message={formik.errors.password}
-            name="password"
-            formik={formik}
-          />
-        </div>
+    <>
+      <div className="auth_section_1">
+        <form className="form_section" onSubmit={handleSubmit}>
+          <div className="form-group mx-4 my-4">
+            <label htmlFor="exampleInputEmail1">UserId</label>
+            <input
+              type="text"
+              className="form-control mt-2 fw-bold"
+              name="userId"
+              onChange={formik.handleChange}
+            />
+            <ErrorMessage
+              message={formik.errors.userId}
+              name="userId"
+              formik={formik}
+            />
+          </div>
+          <div className="form-group mx-4 mt-4 mb-3">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control mt-2 fw-bold"
+              name="password"
+              onChange={formik.handleChange}
+            />
+            <ErrorMessage
+              message={formik.errors.password}
+              name="password"
+              formik={formik}
+            />
+          </div>
 
-        <div className="w-100 d-flex justify-content-center ">
-          <button type="submit" className="btn btn-primary mx-4 my-4 w-100">
-            Login
-          </button>
-        </div>
-      </form>{' '}
-    </div>
+          <div className="w-100 d-flex justify-content-center ">
+            <button
+              type="submit"
+              className="btn btn-primary mx-4 my-4 w-100"
+              disabled={loading.state}
+            >
+              {loading.state ? 'Loading' : 'Submit'}
+            </button>
+          </div>
+        </form>{' '}
+      </div>
+      <Toaster />
+    </>
   );
 }
 
