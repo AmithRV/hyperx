1c467f9510d5f947630403be8932ce458abb5f26
AMITHRAJ V <102154967+amith-mojgenie@users.noreply.github.com>
2024-11-20 11:59:17 +0530

    login page styling

--- src/app/auth/Component.jsx
+++ src/app/auth/Component.jsx
@@ -12,11 +12,11 @@ function FormExample() {
     <form className="form_section" onSubmit={handleSubmit}>
       <div className="form-group mx-4 my-4">
         <label htmlFor="exampleInputEmail1">Email address</label>
-        <input type="email" className="form-control mt-2" />
+        <input type="email" className="form-control mt-2 fw-bold" />
       </div>
       <div className="form-group mx-4 mt-4 mb-3">
         <label htmlFor="exampleInputPassword1">Password</label>
-        <input type="password" className="form-control mt-2" />
+        <input type="password" className="form-control mt-2 fw-bold" />
       </div>
 
       <div className="w-100 d-flex justify-content-center ">
