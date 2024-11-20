ae25a791bd5a60171dac3e0c11715280e08d43ae
AMITHRAJ V <102154967+amith-mojgenie@users.noreply.github.com>
2024-11-20 10:39:29 +0530

    react-bootstap init

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Example from './Component';
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <Example />
+    </div>
+  );
 }
 
 export default page;
