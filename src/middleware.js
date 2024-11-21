8165f2b823d626f05575d89cff1111c3d9291ab4
AMITHRAJ V <102154967+amith-mojgenie@users.noreply.github.com>
2024-11-21 10:56:00 +0530

    middleware.js

---
+++ src/middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = path === '/auth/login' || path === '/auth/signup';
+  const token = request.cookies.get('token')?.value || '';
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
+  }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL('/auth/login', request.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: ['/auth/login', '/auth/signup', '/dashboard'],
+};
