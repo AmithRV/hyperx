43f02e7688b0b15fbe60441a272c8eaa2f4e8927
AMITHRAJ V <102154967+amith-mojgenie@users.noreply.github.com>
2024-12-05 11:42:50 +0530

    commit

--- src/app/stock-filter/components/ItemsTable.jsx
+++ src/app/stock-filter/components/ItemsTable.jsx
@@ -19,74 +19,82 @@ function ItemsTable({
   return (
     <div className={`${isExpanded ? 'w-100' : 'w-75'} items-table`}>
       <Stack direction="horizontal" className="my-4" gap={2}>
-        {headers.map((header, index) => (
-          <Badge
-            pill
-            bg={selectedHeaders.includes(header) ? 'primary' : 'secondary'}
-            key={index}
-            onClick={() => {
-              toggleHeader(header, index);
-            }}
-            onDoubleClick={() => {
-              if (filterKey?.high) {
-                setFilterKey({ ...filterKey, ltp: header });
-              } else {
-                setFilterKey({ ...filterKey, high: header });
-              }
-            }}
-            style={{ cursor: 'pointer' }}
-          >
-            {header}
-          </Badge>
-        ))}
-        {selectedHeaders.length > 0 && !isExpanded && (
-          <Badge
-            bg="danger"
-            onClick={() => {
-              setIsExpanded(true);
-            }}
-            style={{ cursor: 'pointer' }}
-          >
-            ⛶
-          </Badge>
-        )}
-        {selectedHeaders.length > 0 && isExpanded && (
-          <Badge
-            bg="danger"
-            onClick={() => {
-              setIsExpanded(false);
-            }}
-            style={{ cursor: 'pointer' }}
-          >
-            ↕
-          </Badge>
-        )}
+        <div>
+          {headers.map((header, index) => (
+            <Badge
+              pill
+              bg={selectedHeaders.includes(header) ? 'primary' : 'secondary'}
+              key={index}
+              onClick={() => {
+                toggleHeader(header, index);
+              }}
+              onDoubleClick={() => {
+                if (filterKey?.high) {
+                  setFilterKey({ ...filterKey, ltp: header });
+                } else {
+                  setFilterKey({ ...filterKey, high: header });
+                }
+              }}
+              style={{ cursor: 'pointer' }}
+              className="mx-1 my-1"
+            >
+              {header}
+            </Badge>
+          ))}
+        </div>
+
+        <div>
+          {selectedHeaders.length > 0 && !isExpanded && (
+            <Badge
+              bg="danger"
+              onClick={() => {
+                setIsExpanded(true);
+              }}
+              style={{ cursor: 'pointer' }}
+            >
+              ⛶
+            </Badge>
+          )}
+          {selectedHeaders.length > 0 && isExpanded && (
+            <Badge
+              bg="danger"
+              onClick={() => {
+                setIsExpanded(false);
+              }}
+              style={{ cursor: 'pointer' }}
+            >
+              ↕
+            </Badge>
+          )}
+        </div>
       </Stack>
 
-      <Table
-        className={`table table-dark table-striped ${
-          csvData.length === 0 ? 'd-none' : ''
-        }`}
-      >
-        <thead>
-          <tr>
-            <th>#</th>
-            {selectedHeaders.map((e, index) => (
-              <th key={index}>{e}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData?.map((data, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              {selectedHeaders.map((e, i) => (
-                <td key={i}>{data[e]}</td>
+      <div className="table-items-wrap">
+        <Table
+          className={`table table-dark table-striped ${
+            csvData.length === 0 ? 'd-none' : ''
+          }`}
+        >
+          <thead>
+            <tr>
+              <th>#</th>
+              {selectedHeaders.map((e, index) => (
+                <th key={index}>{e}</th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {filteredData?.map((data, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                {selectedHeaders.map((e, i) => (
+                  <td key={i}>{data[e]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 }
