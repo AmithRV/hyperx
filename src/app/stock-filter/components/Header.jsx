c7bf67bf959496dc758906832cb64a74fd799f7c
AMITHRAJ V <102154967+amith-mojgenie@users.noreply.github.com>
2024-12-07 23:46:10 +0530

    commit

--- src/app/stock-filter/components/Header.jsx
+++ src/app/stock-filter/components/Header.jsx
@@ -1,7 +1,23 @@
+import { useRouter } from 'next/navigation';
+
 import '@/styles/stock-filter/header.css';
+import '@/styles/todo-list/header.css';
 
 function Header() {
-  return <div className="header-wrap"></div>;
+  const router = useRouter();
+
+  return (
+    <div className="header-wrap">
+      <div className="loader">
+        <div
+          className="circle"
+          onClick={() => {
+            router.push('/dashboard');
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
