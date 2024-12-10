39537de9dbd94f26107da3023142491939f1c3f3
AMITHRAJ V <102154967+amith-mojgenie@users.noreply.github.com>
2024-12-10 10:50:48 +0530

    task-details design update

--- src/app/todo-list/components/TaskDetails.jsx
+++ src/app/todo-list/components/TaskDetails.jsx
@@ -1,10 +1,12 @@
-import Offcanvas from 'react-bootstrap/Offcanvas';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import Button from 'react-bootstrap/Button';
 import Badge from 'react-bootstrap/Badge';
 import Form from 'react-bootstrap/Form';
-import React from 'react';
 import Image from 'next/image';
+import React from 'react';
+
+import '@/styles/todo-list/task-details.css';
 
 function TaskDetails({
   isVisible = false,
@@ -23,16 +25,29 @@ function TaskDetails({
       placement="end"
     >
       <Offcanvas.Header closeButton>
-        <Offcanvas.Title>{title}</Offcanvas.Title>
+        <Offcanvas.Title>Task Details</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body className="pt-0">
         <div className="d-flex; flex-direction: column;">
           <div className="my-2">
-            <Badge pill bg={status === 'active' ? 'danger' : 'success'}>
-              {status}
-            </Badge>
+            <div className="d-flex justify-content-between align-items-center">
+              <Badge pill bg={status === 'active' ? 'danger' : 'success'}>
+                {status}
+              </Badge>
+
+              <Form.Select
+                aria-label="Default select example"
+                className="mx-4 py-0 bg-black text-white category-select"
+              >
+                <option>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </Form.Select>
+            </div>
 
             <div className="d-flex; flex-direction: column; my-4">
+              <p>{title}</p>
               <InputGroup className="mb-3">
                 <InputGroup.Text
                   id="basic-addon1"
